8749076a55be4f5709e5143a9d6f815d3106d796
theologicalphysicist <adugyamfi3141@gmail.com>
2023-05-04 00:03:06 +0100

    updated logger

--- logger.js
+++ logger.js
@@ -1,7 +1,7 @@
 import chalk from "chalk";
 
 export const RequestLoggerFormat = (tokens, req, res) => {
-    return `\n${chalk.bgHex("#EF596F").bold(" REQ ")} \n${chalk.bold(tokens.method(req, res))} ${chalk.underline(tokens.url(req, res).split("/?")[0])} ${tokens.date(req, res, "web")} \nquery: ${chalk.hex("#EF596F33")(JSON.stringify(req.query, null, 2))} \nbody: ${chalk.hex("#EF596F")(JSON.stringify(req.body, null, 2))} \nparams: ${chalk.hex("#EF596F")(JSON.stringify(req.params, null, 2))}\n`;
+    return `\n${chalk.bgHex("#EF596F").bold(" REQ ")} \n${chalk.bold(tokens.method(req, res))} ${chalk.underline(req.path)}, date:${chalk.italic(tokens.date(req, res, "web"))} \nquery: ${chalk.hex("#EF596F33")(JSON.stringify(req.query, null, 2))} \nbody: ${chalk.hex("#EF596F")(JSON.stringify(req.body, null, 2))} \nparams: ${chalk.hex("#EF596F")(JSON.stringify(req.params, null, 2))}\n`;
 };
 
 
